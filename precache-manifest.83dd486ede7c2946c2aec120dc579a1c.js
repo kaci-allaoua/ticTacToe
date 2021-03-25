8cc6e126625a81bc47b6031b83d7f13e6d07cf4b
kaci-allaoua <allaoua.kaci96@gmail.com>
2021-03-24 23:46:12 -0400

    Updates

---
+++ precache-manifest.83dd486ede7c2946c2aec120dc579a1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea343dc5d7f82cde7d99de122fa2a2b",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "16a5b400d3e2d6a34ef4",
+    "url": "/test/static/css/main.336ec073.chunk.css"
+  },
+  {
+    "revision": "c5fcff42bf7081fbff75",
+    "url": "/test/static/js/2.61e06272.chunk.js"
+  },
+  {
+    "revision": "16a5b400d3e2d6a34ef4",
+    "url": "/test/static/js/main.c577b675.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tic-tac-toe/precache-manifest.19e94b1837530776fe67f83c68fd40e1.js"
+  "/test/precache-manifest.83dd486ede7c2946c2aec120dc579a1c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tic-tac-toe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
